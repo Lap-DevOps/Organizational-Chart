0fe97beb4c082fbbfc93a1606ccf3f0a98211580
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-01-17 21:14:23 +0000

    -- change imports in conftest.py

--- tests/unit/api/conftest.py
+++ tests/unit/api/conftest.py
@@ -8,7 +8,7 @@ from flask import Flask
 from flask.testing import FlaskClient
 from flask_sqlalchemy import SQLAlchemy
 
-from api.app import create_app
+from app import create_app
 
 
 @pytest.fixture
