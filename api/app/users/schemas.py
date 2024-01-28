5c326b94e9354be03bdd78ca72bf76279b88bab2
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-01-28 17:14:18 +0000

    feature: add U user_schema

--- api/app/users/schemas.py
+++ api/app/users/schemas.py
@@ -1,54 +1,42 @@
 """ Module containing user-related Marshmallow schemas."""
+from flask_restx import Namespace, fields
 
-from marshmallow import fields, Schema, validate
+from app.users.models import Role
 
 
-class UserRegistrationSchema(Schema):
-    """
-    Schema for user registration.
+class UserDto:
+    """User data transfer object."""
 
-    Attributes:
-        username: User's username.
-        email: User's email address.
-        password: User's password.
+    api = Namespace("user", description="User related operations")
 
-    Raises:
-        ValidationError: If validation fails.
-    """
-
-    username: str = fields.String(
-        required=True,
-        validate=[
-            validate.Length(min=5, max=120, error="Username must be between 5 and 120 characters."),
-            validate.Regexp(r"^[a-zA-Z0-9_]+$", error="Username can only contain letters, numbers, and underscores"),
-        ],
-        error_messages={
-            "required": "Username is required.",
-            "validator_failed": "Custom message for validator failure.",
-        },
-    )
-    email: str = fields.Email(
-        required=True,
-        validate=[
-            validate.Email(error="Not a valid email address."),
-            validate.Length(min=5, max=120, error="Email must be between 5 and 120 characters."),
-        ],
-        error_messages={
-            "required": "Email is required.",
-            "validator_failed": "Custom message for validator failure.",
-        },
-    )
-    password: str = fields.String(
-        required=True,
-        validate=[
-            validate.Length(min=8, max=256, error="Password must be between 8 and 256 characters."),
-            validate.Regexp(
-                r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$",
-                error="Password must contain at least one uppercase letter, one lowercase letter, and one number",
+    user_schema = api.model(
+        "user_schema",
+        {
+            "username": fields.String(
+                required=True,
+                min_length=3,
+                max_length=120,
+                description="User name",
+                example="username",
+                pattern=r"^[a-zA-Z0-9_]+$",
             ),
-        ],
-        error_messages={
-            "required": "Password is required.",
-            "validator_failed": "Custom message for validator failure.",
+            "email": fields.String(
+                required=True,
+                min_length=6,
+                max_length=120,
+                description="User email address",
+                example="user@test.com",
+                pattern=r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+            ),
+            "role": fields.String(
+                required=True,
+                description="User role",
+                example="guest",
+                enum=[role.name for role in Role],
+                attribute="role",
+                default="guest",
+            ),
+            "employee_id": fields.Integer(required=True, description="User employee id", example="1", default=0),
         },
     )
+
